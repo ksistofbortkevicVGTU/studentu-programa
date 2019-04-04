cbad58f73422391bb9e8b3468459c36742979198
ksistofbortkevicVGTU <ksistof.bortkevic@stud.vgtu.lt>
2019-04-04 15:00:43 +0300

    Sukurtas projektas ir reikalingi failai aplinkai

---
+++ main.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace studentuprograma
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
