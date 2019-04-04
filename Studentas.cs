104eaadccfd39275c4449120755e6eb6cb8cea3f
ksistofbortkevicVGTU <ksistof.bortkevic@stud.vgtu.lt>
2019-04-04 15:40:37 +0300

    Sukurta studento klasė su kintamaisiais ir konstruktoriais

---
+++ Studentas.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace studentuprograma
+{
+    public class Studentas
+    {
+        //Declare variables
+        private string Vardas;
+        private string Pavarde;
+        private double Vidurkis;
+        private List<int> Pazymiai = new List<int>();
+
+        //Declare constructors
+        public Studentas(string vard, string pav)
+        {
+            this.Vardas = vard; this.Pavarde = pav;
+        }
+
+        public Studentas(string vard, string pav, double vid)
+        {
+            this.Vardas = vard; this.Pavarde = pav; this.Vidurkis = vid;
+        }
+
+        //Public methods:
+
+        //Private methods:
+    }
+}
--- main.cs
+++ main.cs
@@ -4,6 +4,11 @@ namespace studentuprograma
 {
     class MainClass
     {
+        private static void Menu()
+        {
+
+        }
+
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
